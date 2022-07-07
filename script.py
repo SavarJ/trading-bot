3212ac3cca98ec070952470dae7db9f3f66e3ff4
Savar Jain <jainsavar@gmail.com>
2022-07-07 16:11:39 -0400

    Add subscription to ticker and pretty printing

--- script.py
+++ script.py
@@ -1,9 +1,19 @@
+import json
 import websocket
+import pprint
 
-SOCKET="wss://ws-feed-public.sandbox.exchange.coinbase.com"
+# SOCKET="wss://ws-feed-public.sandbox.exchange.coinbase.com"
+SOCKET="wss://ws-feed.exchange.coinbase.com"
 
 def on_open(ws):
     print("Websocket connection opened!")
+    ws.send(json.dumps({
+          "type": "subscribe",
+    "product_ids": [
+        "ETH-USD"
+    ],
+    "channels": ["ticker"]
+    }))
 
 def on_close(ws):
     print("Websocket connection closed!")
@@ -12,7 +22,11 @@ def on_error(ws, error):
     print("There was an error with the websocket: " + str(error))
 
 def on_message(ws, message):
-    print("Received message: " + message)
+    print("Received message")
+    json_message = json.loads(message)
+    pprint.pprint(json_message)
+
 
 
 ws = websocket.WebSocketApp(SOCKET, on_open=on_open, on_message=on_message, on_error=on_error, on_close=on_close)
+ws.run_forever()
